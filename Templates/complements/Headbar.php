53a7d9f44a55441a02977e6374f74b77e431cb75
guestuser10 <usuarioguest10@gmail.com>
2022-11-19 23:59:47 -0700

    headbar design

---
+++ Templates/complements/Headbar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Resources/css/headbar.css">
+    <title>SRQ</title>
+
+</head>
+
+<body>
+    <header>
+        <div class="logo" id="logo">
+            <img src="../Resources/media/yuumi.jpg" alt="logo de la fim" class="logo-img" />
+            <h2 class="Facultad-de-ingenieria-mochis"></h2>
+            <nav>
+                <a href="" class="Mis Solicitudes"></a>
+                <a href="" class="Enviar una solicitud"></a>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
